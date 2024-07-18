55755009ccb387cf2a3ac7ec84a2f00ce977ff31
omovc83280 <omovc83280@gmail.com>
2024-07-18 17:40:01 +0900

    修正して提出

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -3,9 +3,5 @@ const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 
 btn.addEventListener('click',() =>{
-  text.style.display='none';
-  const childList = document.createElement('h2');
-  childList.textContent = 'ボタンをクリックしました';
-  const body = document.querySelector("body");
-  body.appendChild(childList);
+  text.textContent='ボタンをクリックしました';
 });
